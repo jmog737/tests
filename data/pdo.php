84ef544cc783a7241d12db799e4ba3254fafe01d
Juan Martin Ortega <jmog737@gmail.com>
2020-09-15 23:27:16 -0300

    Primer commit laptop VSC

--- data/pdo.php
+++ data/pdo.php
@@ -4,7 +4,7 @@ try {
   $pdo = new PDO('mysql:host=localhost;port=3306;dbname=controlstock;charset=utf8',$_SESSION['username'], $_SESSION['username']);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    print "PDO ERROR: " . $e->getMessage() . "<br/>";
+    print "PDO ERROR TEST: " . $e->getMessage() . "<br/>";
     die();
 }
 
